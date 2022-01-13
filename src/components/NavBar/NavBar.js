70aa96487697f286bcd44da450ecac1f0331c06e
C4EJS106DOTHEDAT <dat9d3@gmail.com>
2022-01-13 18:04:00 +0700

    hihi

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,38 +1,55 @@
-import React, {useContext, useEffect, useState} from 'react'
+import { useContext, useEffect, useState } from 'react'
 import "./NavBar.css"
 import logo from "../../assets/banner_logo.png"
-import {Link, NavLink} from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import AuthContext from '../../context/auth'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCreditCard } from '@fortawesome/free-solid-svg-icons'
-import {Badge} from "react-bootstrap"
+import { Badge } from "react-bootstrap"
 import { connect, useDispatch } from 'react-redux'
 import ModalCart from '../HomePage/Modal/ModalCart'
-import { BUYPRODUCT, UPDATECART } from '../../redux/reducer/cart'
+import { UPDATECART } from '../../redux/reducer/cart'
 import ModalSuccess from '../HomePage/Modal/ModalSuccess'
-import ModalBuyInCart from '../HomePage/Modal/ModalBuyInCart'
-const NavBar = ({Infor, CartItems, CartProduct}) => {
-    const authCtx = useContext(AuthContext)
+// import ModalBuyInCart from '../HomePage/Modal/ModalBuyInCart'
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { DropdownButton, Dropdown} from 'react-bootstrap'
+import axios from '../../util/http'
+
+const NavBar = ({ Infor, CartItems, CartProduct }) => {
     const [showModalCart,setShowModalCart] =useState(false)
     const [showModalBuyCart, setShowModalBuyCart] = useState(false)
+    const [clicked, setClicked] = useState(false)
+
+    const authCtx = useContext(AuthContext)
     const dispatch = useDispatch()
+
     const dataShoppingSuccess = JSON.parse(localStorage.getItem("shoppingSuccess")) || []
+
     const updateCart = () => {
         const dataCart = JSON.parse(localStorage.getItem("Cart"))
        
         console.log(dataCart);
-        if(CartItems === 0) {
-            if(dataCart){
+        if (CartItems === 0) {
+            if (dataCart) {
                 dispatch({
                     type: UPDATECART,
                     payload: dataCart
                 })
             }
         }
-
     }
+
     const handleBuyItems = () => {
         setShowModalBuyCart(false)
+    }
+
+    const logout = async () => {
+        try {
+            await axios.post('/auth/logout')
+        } catch (err) {
+            console.log(err);
+        }
+        window.location.reload()
     }
 
     useEffect(() => {
@@ -40,95 +57,164 @@ const NavBar = ({Infor, CartItems, CartProduct}) => {
     },[])
 
     return (
-        <div className="d-flex align-items-center ">
-            <div className='container'>
-                <div className="d-flex justify-content-between align-items-center header-navbar-menu p-3 nav-bar">
-                    <NavLink to='/' className='header-logo '>
-                        <img src={logo} className='logo-img'/>
+        <nav className="d-flex justify-content-between container navigation-bars">
+            <Link to='/' className='header-logo '>
+                <img src={logo} className='logo-img' alt='Logo_Img'/>
+            </Link>
+            {
+                clicked === false ?
+                <FaBars className='navigation-icons' onClick={() => setClicked(true)} /> :
+                <FaTimes className='navigation-icons' onClick={() => setClicked(false)} />
+            }
+            <div className={clicked === false ? 'navigation-bars__menu' : 'navigation-bars__menu__click'}>
+                <NavLink 
+                    to="/gioi-thieu" 
+                    className='navigation-bars__menu__navlink'
+                >
+                    Giới thiệu
+                </NavLink>
+                <NavLink 
+                    to="/huong-dan" 
+                    className='navigation-bars__menu__navlink'
+                >
+                    Hướng dẫn
+                </NavLink>
+                <NavLink 
+                    to={`/tao-the/${authCtx.user?._id}`} 
+                    className={authCtx.user ? 'navigation-bars__menu__navlink' : 'navigation-bars__menu__navlink navigation-bars__menu__navlink__disable'}
+                >
+                    Tạo thẻ
+                </NavLink>
+                {Infor.length !== 0 || dataShoppingSuccess.length !== 0 ?   
+                    <NavLink 
+                        to={authCtx.user ? `/thong-tin-scan/${authCtx.user._id}` : '/login'} 
+                        className='navigation-bars__menu__navlink'
+                    >
+                        Thông tin thẻ
                     </NavLink>
+                    : 
+                    <NavLink 
+                        to={authCtx.user ? `/thong-tin-scan/${authCtx.user._id}` : '/login'} 
+                        className='navigation-bars__menu__navlink disabled'
+                    >
+                        Thông tin thẻ
+                    </NavLink>
+                }
 
-                    {/* <RequireAuth mode="hidden"> */}
-                    {authCtx.user ? 
-                        <>
-                            <ul className="header-menu is-active m-0">
-                                <li className="header-menu-item">
-                                    <Link to="/gioi-thieu" className="header-menu-link text-decoration-none fw-bold fs-6 text">
-                                        Giới thiệu
-                                    </Link>
-                                </li>
-                                <li className="header-menu-item">
-                                    <Link to="/huong-dan" className="header-menu-link text-decoration-none fw-bold fs-6 text">Hướng dẫn</Link>
-                                </li>
-                                <li className="header-menu-item">
-                                    <Link to={`/tao-the/${authCtx.user.id}`} className="header-menu-link text-decoration-none fw-bold fs-6 text">Tạo thẻ</Link>
-                                </li>
-                                {Infor.length !== 0 || dataShoppingSuccess.length !== 0 ?   
-                                    <li className="header-menu-item">
-                                        <Link to={`/thong-tin-scan/${authCtx.user.id}`} className="header-menu-link text-decoration-none fw-bold fs-6 text">Thông tin thẻ</Link>
-                                    </li> 
-                                    : 
-                                    <li className="header-menu-item">
-                                        <Link to={`/thong-tin-scan/${authCtx.user.id}`} className="header-menu-link text-decoration-none fw-bold fs-6 disabled text">Thông tin thẻ</Link>
-                                    </li> 
-                                }
-
-                            </ul>
-                            {/* <span>{authCtx.user.username}</span> */}
-                            <div className='d-flex align-items-center cart'>
-                                <div className='position-relative me-3' onClick={() => setShowModalCart(true)}>
-                                    <FontAwesomeIcon icon={faCreditCard} className=' fs-3'/>
-                                    {CartItems ? <Badge bg="success" className='count-items'>{CartItems}</Badge> : null}
-                                </div>
-                                <div className='p-2 bg-dark text-white rounded'>
-                                    {authCtx.user.username}
-                                </div>
-                            </div>
-                            <ModalCart show={showModalCart} 
-                                onCloseModalCart={() => {
-                                    setShowModalCart(false)
-                                }}
-                                showModalBuyCart={() => {
-                                    setShowModalCart(false)
-                                    setShowModalBuyCart(true)
-                                }}
-                            />
-                            {/* <ModalBuyInCart show={showModalBuyCart} handleCloseModalBuyCart={handleBuyItems}/> */}
-                            <ModalSuccess show={showModalBuyCart} handleCloseModal={handleBuyItems}/>
-                        </>
-                        :
-                        <>
-                            <ul class="header-menu is-active m-0">
-                                <li class="header-menu-item">
-                                    <Link to="/gioi-thieu" className="header-menu-link text-decoration-none fw-bold fs-6 text">
-                                        Giới thiệu
-                                    </Link>
-                                </li>
-                                <li class="header-menu-item">
-                                    <Link to="/huong-dan" className="header-menu-link text-decoration-none fw-bold fs-6 text">Hướng dẫn</Link>
-                                </li>
-                                <li class="header-menu-item">
-                                    {/* <RequireAuth mode="navigate"> */}
-                                        <Link to="/login" className="header-menu-link text-decoration-none fw-bold fs-6 text">Tạo thẻ</Link>
-                                    {/* </RequireAuth> */}
-                                </li>
-                                <li class="header-menu-item">
-                                    <Link to="/login" className="header-menu-link text-decoration-none fw-bold fs-6 text">Thông tin thẻ</Link>
-                                </li>
-                            </ul>
-                            <NavLink to="/login" className="text-decoration-none">
-                                <button className='btn btn-primary'>Tạo thẻ</button>
-                            </NavLink>
-                        </>
-                    }
-                    {/* </RequireAuth> */}
-                </div>
             </div>
-        </div>
+            <div className='d-flex align-items-center'>
+                <div className='position-relative me-3' onClick={() => setShowModalCart(true)}>
+                    <FontAwesomeIcon icon={faCreditCard} className=' fs-3'/>
+                    {CartItems ? <Badge bg="success" className='count-items'>{CartItems}</Badge> : null}
+                </div>
+                {
+                    authCtx.user ?
+                    <DropdownButton
+                        variant="outline-secondary"
+                        title={authCtx.user.username || authCtx.user.user.displayName}
+                    >
+                        <Dropdown.Item>
+                            <NavLink to={`/profile/${authCtx.user._id}`} className='text-decoration-none'>Profile</NavLink>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            <NavLink to={`/edit/${authCtx.user._id}`} className='text-decoration-none'>Edit Card</NavLink>
+                        </Dropdown.Item>
+                        <Link to="/" className='text-decoration-none' >
+                            <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
+                        </Link>
+                    </DropdownButton>
+                    :
+                    <Link to="/login" className="text-decoration-none navigation-button">
+                        <button className='btn btn-primary'>Tạo thẻ</button>
+                    </Link>
+                }
+                <ModalCart show={showModalCart} 
+                    onCloseModalCart={() => {
+                        setShowModalCart(false)
+                    }}
+                    showModalBuyCart={() => {
+                        setShowModalCart(false)
+                        setShowModalBuyCart(true)
+                    }}
+                />
+                <ModalSuccess show={showModalBuyCart} handleCloseModal={handleBuyItems}/>
+            </div>
+            {/* {authCtx.user ? 
+                <>
+                    <ul className="header-menu is-active m-0">
+                        <li className="header-menu-item">
+                            <Link to="/gioi-thieu" className="header-menu-link text-decoration-none fw-bold fs-6 text">
+                                Giới thiệu
+                            </Link>
+                        </li>
+                        <li className="header-menu-item">
+                            <Link to="/huong-dan" className="header-menu-link text-decoration-none fw-bold fs-6 text">Hướng dẫn</Link>
+                        </li>
+                        <li className="header-menu-item">
+                            <Link to={`/tao-the/${authCtx.user.id}`} className="header-menu-link text-decoration-none fw-bold fs-6 text">Tạo thẻ</Link>
+                        </li>
+                        {Infor.length !== 0 || dataShoppingSuccess.length !== 0 ?   
+                            <li className="header-menu-item">
+                                <Link to={`/thong-tin-scan/${authCtx.user.id}`} className="header-menu-link text-decoration-none fw-bold fs-6 text">Thông tin thẻ</Link>
+                            </li> 
+                            : 
+                            <li className="header-menu-item">
+                                <Link to={`/thong-tin-scan/${authCtx.user.id}`} className="header-menu-link text-decoration-none fw-bold fs-6 disabled text">Thông tin thẻ</Link>
+                            </li> 
+                        }
+
+                    </ul>
+                    <div className='d-flex align-items-center cart'>
+                        <div className='position-relative me-3' onClick={() => setShowModalCart(true)}>
+                            <FontAwesomeIcon icon={faCreditCard} className=' fs-3'/>
+                            {CartItems ? <Badge bg="success" className='count-items'>{CartItems}</Badge> : null}
+                        </div>
+                        <div className='p-2 bg-dark text-white rounded'>
+                            {authCtx.user.username}
+                        </div>
+                    </div>
+                    <ModalCart show={showModalCart} 
+                        onCloseModalCart={() => {
+                            setShowModalCart(false)
+                        }}
+                        showModalBuyCart={() => {
+                            setShowModalCart(false)
+                            setShowModalBuyCart(true)
+                        }}
+                    />
+                    <ModalSuccess show={showModalBuyCart} handleCloseModal={handleBuyItems}/>
+                </>
+                :
+                <>
+                    <ul class="header-menu is-active m-0">
+                        <li class="header-menu-item">
+                            <Link to="/gioi-thieu" className="header-menu-link text-decoration-none fw-bold fs-6 text">
+                                Giới thiệu
+                            </Link>
+                        </li>
+                        <li class="header-menu-item">
+                            <Link to="/huong-dan" className="header-menu-link text-decoration-none fw-bold fs-6 text">Hướng dẫn</Link>
+                        </li>
+                        <li class="header-menu-item">
+                                <Link to="/login" className="header-menu-link text-decoration-none fw-bold fs-6 text">Tạo thẻ</Link>
+                        </li>
+                        <li class="header-menu-item">
+                            <Link to="/login" className="header-menu-link text-decoration-none fw-bold fs-6 text">Thông tin thẻ</Link>
+                        </li>
+                    </ul>
+                    <NavLink to="/login" className="text-decoration-none">
+                        <button className='btn btn-primary'>Tạo thẻ</button>
+                    </NavLink>
+                </>
+            } */}
+        </nav>
     )
 }
-const maptoStatetoProps = (state) => ({
+
+const maptoStatetoProps = state => ({
     Infor: state.Infor.data,
     CartItems: state.Cart.items,
     CartProduct: state.Cart.product
 })
+
 export default connect(maptoStatetoProps, null)(NavBar)
